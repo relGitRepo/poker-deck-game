1bba58ab78a96511dc5456bf7ff4e4424f099e4a
Raja El <relGitRepo@gmail.com>
2022-09-17 15:46:50 -0400

    Add initial directory structure and files

---
+++ src/test/java/com/poker/deck/PokerDeckGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poker.deck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerDeckGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
